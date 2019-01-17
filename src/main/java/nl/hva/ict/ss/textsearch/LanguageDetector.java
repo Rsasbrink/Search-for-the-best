5ffa45d1ee58477b57b2f104af063a754b8eefcd
René Sasbrink <R.sasbrink@live.nl>
2019-01-17 11:49:13 +0100

    made lowercase and checked all letters frequencies

--- src/main/java/nl/hva/ict/ss/textsearch/LanguageDetector.java
+++ src/main/java/nl/hva/ict/ss/textsearch/LanguageDetector.java
@@ -1,15 +1,48 @@
 package nl.hva.ict.ss.textsearch;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class LanguageDetector {
+public final class LanguageDetector {
+
     private String content; // Once an instance is created this will hold the complete content of the file.
+    HashMap<Character, Integer> letterFrequency = new HashMap<>();
 
     public LanguageDetector(InputStream input) {
         Scanner sc = new Scanner(input);
         sc.useDelimiter("\\Z"); // EOF marker
         content = sc.next();
+
+    }
+
+    public boolean isEnglish() {
+        content = convertToLowerCase(content);
+        getLetterFrequency();
+        System.out.println(letterFrequency);
+        return true;
+    }
+
+    public void getLetterFrequency() {
+        for (int i = 0; i < content.length(); i++) {
+            char c = content.charAt(i);
+            if (Character.isLetter(c)) {
+                Integer val = letterFrequency.get(c);
+                if (val != null) {
+                    letterFrequency.put(c, new Integer(val + 1));
+                } else {
+                    letterFrequency.put(c, 1);
+                }
+            }
+
+        }
+
+    }
+
+    public String convertToLowerCase(String content) {
+        String lowerContent;
+        lowerContent = content.toLowerCase();
+        return lowerContent;
     }
 
     // Put your own code here and integrate it with the test class.
--- src/test/java/nl/hva/ict/ss/textsearch/LanguageDetectorTest.java
+++ src/test/java/nl/hva/ict/ss/textsearch/LanguageDetectorTest.java
@@ -1,6 +1,8 @@
 package nl.hva.ict.ss.textsearch;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
+import org.junit.Test;
 
 public class LanguageDetectorTest {
     private LanguageDetector detector;
@@ -8,7 +10,11 @@ public class LanguageDetectorTest {
     @Before
     public void setup() {
         detector = new LanguageDetector(getClass().getResourceAsStream("/edu/princeton/cs/algs4/Huffman.java"));
-    }
-
+         
+   }
     // Add your tests here. They are allowed to NOT use assertXxxx... :-)
+    @Test
+    public void cantFindOccurrence() {
+     detector.isEnglish();
+    }
 }
